3fae42669fe4026fee501ec82eadbe95cdd047f2
Frosty515 <frosty515studiosyt@gmail.com>
2022-08-03 12:21:33 +1000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,168 @@
+<<<<<<< HEAD
+workspace "Frosty515"
+    architecture "x64"
+    startproject "Compiler"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "StandardFileIO"
+    location "StandardFileIO"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir)
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "StandardLogger/src"
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "F515_PLATFORM_WINDOWS",
+            "F515_BUILD_DLL"
+        }
+
+    filter "system:linux"
+        cppdialect "C++2a"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "F515_PLATFORM_LINUX",
+            "F515_BUILD_SO"
+        }
+
+    filter "configurations:Debug"
+        defines "F515_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "F515_RELEASE"
+        optimize "On"
+
+project "StandardLogger"
+    location "StandardLogger"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir)
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "StandardFileIO/src"
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "F515_PLATFORM_WINDOWS",
+            "F515_BUILD_DLL"
+        }
+
+    filter "system:linux"
+        cppdialect "C++2a"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "F515_PLATFORM_LINUX",
+            "F515_BUILD_SO"
+        }
+
+    filter "configurations:Debug"
+        defines "F515_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "F515_RELEASE"
+        optimize "On"
+
+project "Compiler"
+    location "Compiler"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir)
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "StandardFileIO/src",
+        "StandardLogger/src"
+    }
+
+    links
+    {
+        "StandardFileIO",
+        "StandardLogger"
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "F515_PLATFORM_WINDOWS"
+        }
+
+    filter "system:linux"
+        cppdialect "C++2a"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "F515_PLATFORM_LINUX"
+        }
+
+    filter "configurations:Debug"
+        defines "F515_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "F515_RELEASE"
+        optimize "On"
+=======
 workspace "Frosty515"
     architecture "x64"
     startproject "Testing"
@@ -140,3 +305,4 @@ project "Testing"
     filter "configurations:Release"
         defines "F515_RELEASE"
         optimize "On"
+>>>>>>> d71c144367729a1ee6598edeb36e04e1e999b19f
